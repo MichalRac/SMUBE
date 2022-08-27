904ad81995665f8170a939c4041d2aad2207402e
MichalRac <rac.michalmail@gmail.com>
2022-08-27 02:35:41 +0200

    Setup SMUBE project with initial solution and projects

---
+++ SMUBE/Units/Core/Unit.cs
@@ -0,0 +1,19 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Units.Core
+{
+    internal class Unit
+    {
+        public string name;
+        public int health;
+        public int maxHealth;
+
+        public int posX;
+        public int posY;
+    }
+}
---
+++ SMUBE/Units/Skills/Skill.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Units.Core
+{
+    internal class Skill
+    {
+        public int id;
+        public string name;
+    }
+}
